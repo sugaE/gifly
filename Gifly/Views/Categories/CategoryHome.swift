806579a7211397823226e7d741a317c080a98ab2
suga <suga_e@outlook.com>
2020-12-16 20:22:48 +0800

    [swift tutorial] Composing Complex Interfaces

---
+++ Gifly/Views/Categories/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  Gifly
+//
+//  Created by 女王様 on 2020/12/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) {key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
